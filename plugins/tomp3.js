0ae9ae182fa9f6a2d89c61682e6dbd3a7f4a7ace
nizartitwaniii <90058773+nizartitwaniii@users.noreply.github.com>
2023-09-01 13:07:58 +0100

    Update tomp3.js

--- plugins/tomp3.js
+++ plugins/tomp3.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
    /* let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
     if (!/video|audio/.test(mime)) throw `🇱🇰 Mention to 💝 Queen Hentai 💝 a video or document for convert to mp3,with the command :\n\n*${usedPrefix + command}*`*/
     let media = await q.download?.()
-    if (!media) throw 'حذثت مشكلة راسل صاحب البوت \n instagram.com/noureddine_ouafy'
+    if (!media) throw 'حذثت مشكلة راسل صاحب البوت \n https://www.facebook.com/nizar.primo.946'
     let audio = await toAudio(media, 'mp4')
     if (!audio.data) throw ' Error'
     conn.sendFile(m.chat, audio.data, 'audio.mp3', '', m, null, { mimetype: 'audio/mp4' })
