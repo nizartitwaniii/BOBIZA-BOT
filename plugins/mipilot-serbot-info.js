4e5077cd3d5e75b12fc7ac7c894171d4564645c1
nizartitwaniii <90058773+nizartitwaniii@users.noreply.github.com>
2023-09-01 13:23:53 +0100

    Update mipilot-serbot-info.js

--- plugins/mipilot-serbot-info.js
+++ plugins/mipilot-serbot-info.js
@@ -1,6 +1,6 @@
 async function handler(m, { usedPrefix }) {
 let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user)])]
-await m.reply( '*—◉ instagram.com/noureddine_ouafy*')
+await m.reply( '*—◉ https://www.facebook.com/nizar.primo.946*')
 await m.reply(users.map(v => '👉🏻 wa.me/' + v.jid.replace(/[^0-9]/g, '') + `?text=${usedPrefix}estado (${v.name})`).join('\n'))}
 handler.command = handler.help = ['listjadibot','bots','subsbots']
 handler.tags = ['jadibot']
